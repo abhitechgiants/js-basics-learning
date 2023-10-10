b5dea9b665180356591ce0b866bfeddb423c1266
abhitechgiants <109287613+abhitechgiants@users.noreply.github.com>
2023-10-10 17:19:35 +0000

    new learning

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let isnumber = String(score)
+// console.log(typeof isnumber);
+// console.log(isnumber);
+
+let Name = "33"
+
+console.log(typeof Name);
+
+let valueisNumber = Number(Name)
+console.log(typeof valueisNumber);
+console.log(valueisNumber);
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);

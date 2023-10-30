b70d00998ad753ac23ff24c70894699572054451
abhitechgiants <109287613+abhitechgiants@users.noreply.github.com>
2023-10-30 12:18:23 +0000

    basics js arrays and methods

---
+++ 02_basics/01_array.js
@@ -0,0 +1,37 @@
+//Arrays
+
+const myArray = [1,2,3,4,5,6]
+const myHeroes = ["ironman", "thor"]
+const myArray2 = new Array(1,2,3)
+
+//console.log(myArray[2]);
+
+//Arrays methods
+
+// myArray.push(7)
+// myArray.push(8)
+// myArray.pop()
+// myArray.unshift(0)
+// myArray.shift()
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(4));
+
+// const newArray = myArray.join()
+// console.log(myArray);
+// console.log(newArray);
+// console.log(typeof newArray);
+
+console.log("A ",myArray);
+
+const myn1 = myArray.slice(1,3)
+console.log("B ", myArray);
+console.log(myn1);
+
+
+const myn2 = myArray.splice(1,3)
+console.log("C ", myArray);
+console.log(myn2);
+
+
+
